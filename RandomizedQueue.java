6aa9c80dd6324f62d1c3f4190fec13a275d6662e
Viktor Laishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-05-10 17:04:12 +0200

    Update RandomizedQueue.java

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,7 +1,7 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;   
 import edu.princeton.cs.algs4.StdRandom;   
- 
+  
 public class RandomizedQueue<Item> implements Iterable<Item> { 
     private int n = 1;  
     private Item[] items; 
