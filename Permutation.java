0bdc29e58118d01b689bf8280e52eda63f563fb0
Viktor Laishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-05-19 21:07:58 +0200

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -4,7 +4,7 @@ public class Permutation {
     public static void main(String[] args) {  
         RandomizedQueue<String> que = new RandomizedQueue<>();  
         int k = Integer.parseInt(args[0]); 
-        while (!StdIn.isEmpty()) {  
+        while (!StdIn.isEmpty()) {   
             que.enqueue(StdIn.readString());   
         }
         for (int i = 0; i < k; i++) {
