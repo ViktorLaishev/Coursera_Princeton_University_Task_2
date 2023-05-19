9f67b76741eeac0206f85fb54af80924e46f83b3
Viktor Laishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-05-19 21:07:51 +0200

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -1,7 +1,7 @@
 import edu.princeton.cs.algs4.StdIn;
  
 public class Permutation { 
-    public static void main(String[] args) {
+    public static void main(String[] args) {  
         RandomizedQueue<String> que = new RandomizedQueue<>();  
         int k = Integer.parseInt(args[0]); 
         while (!StdIn.isEmpty()) {  
